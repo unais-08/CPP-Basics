af39b0858a9d5de9bcbaaad21209689d451bcf4c
Unais Shaikh <unais.shaikh.dev@gmail.com>
2024-01-13 20:06:12 +0530

    arrays

---
+++ arrays.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int A[3];
+    A[0]=0;
+    cout<<A[0];
+    cout<<A[1];
+    cout<<A[2];
+    cout<<A[3];
+    cout<<A[4];
+    cout<<endl;
+    // int B[5]={1,2,3,4,5};
+    // cout<<B[0]<<" ";
+    // cout<<B[1]<<" ";
+    // cout<<B[2]<<" ";
+    // cout<<B[3]<<" ";
+    // cout<<B[4]<<" ";
+    // cout<<B[5]<<" ";
+    // cout<<B[6]<<" ";
+    // cout<<B[7]<<" ";
+    // cout<<B[8]<<" ";
+    // cout<<B[9]<<" ";
+    int c[10]={1};
+    for(int i = 0 ; i<10 ; i++){
+        cout<<c[i]<<" ";
+    }
+}
