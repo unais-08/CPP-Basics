c756b9ebc2f7a6629a91fd05aeceb28649d545bf
Unais Shaikh <unais.shaikh.dev@gmail.com>
2024-02-14 15:55:35 +0530

    portfolio changes

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,16 +1,19 @@
-const navmenu = document.querySelector(".navmenu");
+const navmenu = document.getElementById("navmenu");
 const btn = document.querySelector(".hamburgerMenu");
-
-function crossSymbol(){
+var navbar =document.getElementById('navmenu');
+function crossSymbol() {
     btn.classList.toggle("iconclass");
 }
-btn.addEventListener("click", ()=>{
+
+
+btn.addEventListener("click", () => {
     const datavisible = navmenu.getAttribute("data-hidden");
-    
-    if(datavisible ==="true"){
-        navmenu.setAttribute("data-hidden",false);
+    navbar.classList.toggle('visible');
+
+    if (datavisible === "true") {
+        navmenu.setAttribute("data-hidden", false);
     }
-    else{
-        navmenu.setAttribute("data-hidden",true);
+    else {
+        navmenu.setAttribute("data-hidden", true);
     }
 });
